0860494d74eee69dc3b02d48c1b4bbc34217fee4
Argha Mondal <arghamondal888@gmail.com>
2025-06-28 21:43:31 +0530

    Add files via upload

---
+++ list_method.py
@@ -0,0 +1,25 @@
+
+list =[8,5,6,7]
+
+'''list.append(2)    
+print(list)
+print(list.sort()  )  #using sort
+
+print(list)
+
+print(list.sort(reverse=True))  #using sort in decending order
+# print(list) '''
+
+print(list.reverse()) #using reverse method
+print(list)
+
+print(list.insert(2,10))  #using  add element on a particular position
+print(list)
+
+
+print(list.remove(6))   #remove any elements on a lists
+print(list)
+
+
+print(list.pop(1))
+print(list)
---
+++ list_slicing.py
@@ -0,0 +1,7 @@
+
+marks =[23,54,89,86,78,95]
+
+
+print(marks[1:4])
+print(marks[:4])
+print(marks[1:len(marks)])
